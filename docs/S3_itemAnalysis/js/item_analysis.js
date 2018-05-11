2b9c659e6426612aaef0a7559e84f5eebfef27ea
madebyafox <amyraefox@gmail.com>
2018-05-10 23:54:21 -0700

    relative paths

--- docs/S3_itemAnalysis/js/item_analysis.js
+++ docs/S3_itemAnalysis/js/item_analysis.js
@@ -19,8 +19,8 @@ var mymappings ;
 
 //LOAD DATA
 d3.queue()
-    .defer(d3.json, "/S3_itemAnalysis/data/final_items.json")
-    .defer(d3.csv, "/S3_itemAnalysis/data/recordings_mapping.csv")
+    .defer(d3.json, "../S3_itemAnalysis/data/final_items.json")
+    .defer(d3.csv, "../S3_itemAnalysis/data/recordings_mapping.csv")
     .await(visualize);
 
 function filter(data){
