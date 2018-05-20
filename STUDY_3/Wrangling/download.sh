efec0c65906c249090a77227b49cdb54b17e3a9b
madebyafox <amyraefox@gmail.com>
2018-05-20 02:25:43 -0700

    update wranglinginstructions

--- STUDY_3/Wrangling/download.sh
+++
@@ -1,3 +0,0 @@
-SESSION=$1
-
-mongoexport -h ds259325.mlab.com:59325 -d 2ypdb-s3-beh -c entries -u expadmin -p thirdyear --out $1.json
---
+++ STUDY_3/Wrangling/session_files/Replication/note.md
@@ -0,0 +1,1 @@
+NOTE: first.json contains data from first week of data collection, including both SGC3 and SGC5
