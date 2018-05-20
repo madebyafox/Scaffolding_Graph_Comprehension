92536a82952d5355059b8f0e0fb88de592bd5a4d
madebyafox <amyraefox@gmail.com>
2018-05-20 02:55:57 -0700

    restructure backups

---
+++ STUDY_5/Data/1-session_exports/SP18/note.md
@@ -0,0 +1,1 @@
+NOTE: first.json in STUDY_3 contains data from first week of data collection, including both SGC3, SGC3_Narrative and SGC5
---
+++ STUDY_5/Data/1-session_exports/download.sh
@@ -0,0 +1,4 @@
+SESSION=$1
+
+mongoexport -h ds259325.mlab.com:59325 -d 2ypdb-s3-beh -c entries -u expadmin -p thirdyear --out $1.json
+mongoimport -d SGC3 -c pilots --file $1.json
